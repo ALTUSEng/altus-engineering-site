97768cc63284eae44e05c7906354d5feb06cff25
ALTUSEng <trent.mcdonald@altusengineering.com.au>
2026-04-22 14:50:56 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function submitEnquiry(event){
+  event.preventDefault();
+  alert('This is the local preview version. Once you approve the design, the enquiry form can be connected to your business email using a free form handler.');
+  return false;
+}
